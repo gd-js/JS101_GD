b4f36a8e5b8a44e92ec4cc1ed7edfd24044f2281
Dennis Burkhard <germandev.js@protonmail.ch>
2021-03-06 08:07:41 +0000

    added readline-sync functionality

---
+++ DIR2/calculator.js
@@ -0,0 +1,6 @@
+// Ask the user for the first number.
+// Ask the user for the second number.
+// Ask the user for an operation to perform.
+// Perform the operation on the two numbers.
+// Print the result to the terminal.
+
