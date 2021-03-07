1be524d9dfac03d7fef7caa71704cb57511abe53
Dennis Burkhard <germandev.js@protonmailch>
2021-03-07 20:29:02 +0000

    Completed Calculator functionality

--- DIR2/calculator.js
+++ DIR2/calculator.js
@@ -4,3 +4,37 @@
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 
+/// SET UP ///
+const readline = require("readline-sync");
+
+
+/// INITIAL USER INTERACTION ///
+console.log("Welcome to the fabulous Mrs. Calculatora!");
+
+
+/// FUNCTIONALITY ///
+console.log("What is the first number?");
+let number1 = readline.question();
+
+console.log("What is the second number?");
+let number2 = readline.question();
+
+console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+
+
+let operation = readline.question();
+
+
+
+let output;
+if (operation === '1') { // '1' represents addition
+  output = Number(number1) + Number(number2);
+} else if (operation === '2') { // '2' represents subtraction
+  output = Number(number1) - Number(number2);
+} else if (operation === '3') { // 3 represents multiplication
+  output = Number(number1) * Number(number2);
+} else if (operation === '4') { // 4 represents division
+  output = Number(number1) / Number(number2);
+}
+
+console.log(`The result is: ${output}`);
