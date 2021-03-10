3826ebca0b5f0d223ce5bc96ee6c79dfb2e07869
Dennis Burkhard <germandev.js@protonmail.ch>
2021-03-10 07:05:32 +0000

    prompt function to alter message

--- DIR2/calculator.js
+++ DIR2/calculator.js
@@ -1,14 +1,18 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to fabulous Calculator!');
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log("What's the first number?");
+prompt('Welcome to fabulous Calculator!');
+
+prompt("What's the first number?");
 let number1 = readline.question();
 
-console.log("What's the second number?");
+prompt("What's the second number?");
 let number2 = readline.question();
 
-console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
 let operation = readline.question();
 
 let output;
@@ -22,4 +26,4 @@ if (operation === '1') { // '1' represents addition
   output = Number(number1) / Number(number2);
 }
 
-console.log(`The result is: ${output}`);
+prompt(`The result is: ${output}`);
--- DIR2/primecalc.js
+++ DIR2/primecalc.js
@@ -18,25 +18,25 @@ operation = readline.question();
 
 let output;
 if  (operation === "1") {
-    output = Number(number1) ** Number(number2);
+  output = Number(number1) ** Number(number2);
 
 } else if
   (operation === "2") {
-    output = Number(number1) % Number(number2);
+  output = Number(number1) % Number(number2);
   }
 
 
 console.log(`the output is ... .. . \n\n${output}\n\n`);
 
 for (let i = 2; i < output; i++) {
-  if (output % i == 0) {
-    isPrime = false;
-    break;
+  if (output % i === 0) {
+  isPrime = false;
+  break;
   }
 }
 
 if (isPrime) {
-    console.log(`${output} is a prime number`);
+  console.log(`${output} is a prime number`);
 } else {
-    console.log(`${output} is a not prime number`);
+  console.log(`${output} is a not prime number`);
 }
