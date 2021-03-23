38cb77d0f545ccf917afcfdea03a7973ef20f296
Dennis Burkhard <germandev.js@protonmail.ch>
2021-03-23 14:35:03 +0000

    More input choices

--- DIR2/rps.js
+++ DIR2/rps.js
@@ -1,19 +1,59 @@
 /// CONSTANTS AND FUNCTIONS ///
+///to add: best of five
+///to add: letter input
 
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const VALID_CHOICES = ['rock','r','paper','p','scissors','sc','lizard','l','spock','sp'];
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function displayWinner(choice, computerChoice) {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+function letterToName(choiceInput) {
+  switch (choiceInput) {
+  case "r":
+    choice = "Rock";
+    computerChoice = "Rock";
+    return choice;
+    return computerChoice;
+  case "p": 
+    choice = "Paper";
+    computerChoice = "Paper";
+    return choice;
+    return computerChoice;
+    break;
+  case "sc":
+    choice = "Scissors";
+    computerChoice = "Scissors";
+    return choice;
+    return computerChoice;
+    break;
+  case "l":
+    choice = "Lizard";
+    computerChoice = "Lizard";
+    return choice;
+    return computerChoice;
+    break;
+  case "sp":
+    choice = "Spock";
+    computerChoice = "Spock";
+    return choice;
+    return computerChoice;
+    break;
+  case "rock" || "paper" || "scissors" || "lizard" || "spock":
+    return inputChoice;
+    break; 
+  }
+}
 
-  if ((choice === 'rock' && (computerChoice === ('scissors' || 'lizard'))) ||
-      (choice === 'paper' && (computerChoice === ('rock' || 'spock'))) ||
-      (choice === 'scissors' && (computerChoice === ('paper'|| 'lizard'))) ||
-      (choice === 'spock' && (computerChoice === ('scissors' || 'rock')))) {
+function displayWinner(choice, computerChoice) {
+  prompt(`You chose ${letterToName(choice)}, computer chose ${letterToName(computerChoice)}`);
+
+  if (((choice === ('rock' || 'r') && (computerChoice === ('scissors' || 'sc' || 'lizard' || 'l')))) ||
+      ((choice === ('paper' || 'p') && (computerChoice === ('rock' || 'r' || 'spock' || 'sp')))) ||
+      ((choice === ('scissors' || 'sc') && (computerChoice === ('paper' || 'p' || 'lizard' || 'l')))) ||
+      ((choice === ('spock' || 'sp') && (computerChoice === ('scissors' || 'sc' || 'rock' || 'r')))) ||
+      ((choice === ('lizard' || 'l') && (computerChoice === ('spock' || 'sp' || 'paper' || 'p'))))){
     prompt('You win!');
   } else if (choice === computerChoice) {
     prompt('It\'s a tie!');
@@ -22,30 +62,29 @@ function displayWinner(choice, computerChoice) {
   }
 }
 
-/// LOOPS ///
+/// LOOP ///
 
-let answer = 'true';
-
-while (answer !== false ) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
+while (true) {
+  prompt('Choose one: Rock/r, Paper/p, Scissors/sc, Lizard/l or Spock/sp');
+  let choice = readline.question().toLowerCase();
 
   while (!VALID_CHOICES.includes(choice)) {
     prompt("That's not a valid choice");
     choice = readline.question();
   }
 
-  /// WINNER CALCULATION AND PLAY AGAIN OPTIONALITY ///
-
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let randomIndex = Math.floor(Math.random() * (VALID_CHOICES.length - 5));
   let computerChoice = VALID_CHOICES[randomIndex];
 
   displayWinner(choice, computerChoice);
 
   prompt('Do you want to play again (y/n)?');
-  answer = readline.question().toLowerCase();
-  while (answer !== 'n' && answer !== 'y') {
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
     prompt('Please enter "y" or "n".');
     answer = readline.question().toLowerCase();
   }
+
+  if (answer[0] !== 'y') break;
+
 }
